9bd0f7e9fba1461f1ce674037897ac18cb3bed7f
TlXE <92133429+TlXE@users.noreply.github.com>
2022-12-11 09:52:46 -0600

    Add files via upload

---
+++ Portafolio_4/Metodos.py
@@ -0,0 +1,14 @@
+class Droid:
+    def switch_on(self):
+        print("Hi! I'm a droid. Can I help you?")
+
+    def switch_off(self):
+        print("Bye! I'm going to sleep")
+
+
+k2so = Droid()
+
+k2so.switch_on()
+
+
+k2so.switch_off()
---
+++ Portafolio_4/atributos.py
@@ -0,0 +1,9 @@
+blue_droid = StarWarsDroid()
+golden_droid = StarWarsDroid()
+
+golden_droid.name = 'C-3PO'
+
+blue_droid.name = 'R2-D2'
+blue_droid.height = 1.09
+blue_droid.num_feet = 3
+blue_droid.partner_droid = golden_droid 
