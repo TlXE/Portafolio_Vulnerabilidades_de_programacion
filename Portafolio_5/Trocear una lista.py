a509d9ff442f7f8919ce1c40b92b71ea85ebdb40
TlXE <92133429+TlXE@users.noreply.github.com>
2022-12-11 09:55:53 -0600

    Add files via upload

---
+++ Portafolio_5/Trocear una lista.py
@@ -0,0 +1,13 @@
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+
+shopping[0:3]
+['Agua', 'Huevos', 'Aceite']
+
+shopping[:3]
+['Agua', 'Huevos', 'Aceite']
+
+shopping[2:4]
+['Aceite', 'Sal']
+
+shopping[-1:-4:-1]
+['Limón', 'Sal', 'Aceite']
